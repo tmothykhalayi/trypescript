cb3f0289e0e3c707767276016daff5077f3549c9
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-02-12 15:46:16 +0300

    update

---
+++ objects.arrays.ts
@@ -0,0 +1,15 @@
+let names = ['Timothy', 'Edwin', 'Thaddeaus'];
+names.push('Khalayi');
+
+let numbers = [12, 34, 67, 23, 93, 12];
+numbers.push(455555);
+
+let mixed = ['KEN', 234, 23456, 'EDWARD', 'IRENE'];
+mixed.push(123, 'teyla');
+mixed[0] = 45;
+
+let Nandwa = {
+    Name: 'timothy',
+    Age: 34,
+    belt: 'black',
+};
