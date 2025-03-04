f9be0cfa07fa4b516cde9623e6d5d25c6e3b1d28
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-03-04 11:33:30 +0300

    update

---
+++ src/basics.ts
@@ -0,0 +1,13 @@
+let myname:string='timothy'
+myname='timothy khalayi'
+let meaningoflife:number=43;
+let album:any ='hellen'
+let isloggedin:boolean =true
+
+
+const add = (a:number ,b:number) =>{
+    return a+b
+}
+let postid:string|number ='167'
+
+let isactive:number|Boolean
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+let a:number =12;
+let b:number =6;
+let c:number =a+b;
+let sum:number =(a + b +c);
+console.log(sum);
