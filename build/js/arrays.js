9e9f07863ba6a6de04f15853ff2db5d48b7a1337
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-03-04 12:22:35 +0300

    update

---
+++ build/js/arrays.js
@@ -0,0 +1,7 @@
+"use strict";
+//first way of declaring arrays
+//let numbers: number[] = [1, 2, 3, 4];
+//let fruits: string[] = ['apple', 'orange', 'banana'];
+//second array declaration
+//let numbers: Array<number> = [1, 2, 3, 4];
+//let fruits: Array<string> = ['apple', 'orange', 'banana'];
--- src/objects.arrays.ts
+++ src/objects.arrays.ts
@@ -4,7 +4,7 @@ names.push('Khalayi');
 let numbers = [12, 34, 67, 23, 93, 12];
 numbers.push(455555);
 
-let mixed = ['KEN', 234, 23456, 'EDWARD', 'IRENE'];
+let mixeddata = ['KEN', 234, 23456, 'EDWARD', 'IRENE'];
 mixed.push(123, 'teyla');
 mixed[0] = 45;
 
@@ -13,3 +13,30 @@ let Nandwa = {
     Age: 34,
     belt: 'black',
 };
+
+let stringArr = [ ];
+let test =[ ]
+let bands: string[] = [];
+bands.push('timothy')
+
+let myobj:object;
+
+
+const  exampleobject ={
+    prop1 : true ,
+    prop2 : 'dev'
+
+};
+exampleobject.prop2 ='john'
+type guitarist = {
+    name: string ,
+    activestatus: boolean ,
+    albums: [string, boolean][]
+  }
+  
+  let myd = {
+    name: "edu",
+    activestatus: true,
+    albums: [["eland", true]]
+  }
+  
