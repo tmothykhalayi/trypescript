da31ae16f11b7a7ca4780cf78a33cfc282b42924
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-03-13 15:20:07 +0300

    update

--- src/OOP.ts
+++ src/OOP.ts
@@ -1,21 +1,21 @@
-class Person{
-   public name:string = '';
-    public Age:number = 0;
-    constructor(_name:string ,_Age:number ){
-        this.Age =_Age;
-        this.name =_name;
-    }
+// class Person{
+//    public name:string = '';
+//     public Age:number = 0;
+//     constructor(_name:string ,_Age:number ){
+//         this.Age =_Age;
+//         this.name =_name;
+//     }
     
-    greet():string{
-        return'hello world!';
-    }
-}
+//     greet():string{
+//         return'hello world!';
+//     }
+// }
 
-const  Person1 =new Person( 'Timothy' ,25)
-const  Person2 =new Person( 'john' ,2345)
+// const  Person1 =new Person( 'Timothy' ,25)
+// const  Person2 =new Person( 'john' ,2345)
 
-console.log(Person1.Age);
-console.log(Person1.greet());
+// console.log(Person1.Age);
+// console.log(Person1.greet());
 
-console.log(Person2.Age);
-console.log(Person2.greet());
+// console.log(Person2.Age);
+// console.log(Person2.greet());
